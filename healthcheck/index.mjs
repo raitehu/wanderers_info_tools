cedee0b7dce12e7732e272ccdd77b66c7200c075
SHIMOTORI Masanari <shimotori.masanari@gmail.com>
2024-09-14 09:39:39 +0900

    :sparkles: add healtchcheck function

---
+++ healthcheck/index.mjs
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const fqdn = process.env.WANDERERS_INFO_BACKEND_URL;
+const res = await axios.get(`${fqdn}/v1/healthcheck`);
+
+export async function handler() {
+  console.log(res.data);
+  return res.data;
+}
+
+// https://wanderers-info-backend.herokuapp.com/v1/healthcheck
