6ae4af471b4b3c5dc2b578c7d2f94dc05d7d5141
SHIMOTORI Masanari <shimotori.masanari@gmail.com>
2024-10-08 01:20:12 +0900

    :rocket: 一通りの実装完了

--- new-arrival-notifier/twitter.mjs
+++ new-arrival-notifier/twitter.mjs
@@ -5,10 +5,10 @@ import Twitter from "twitter-api-v2";
   const endpoint = "http://localhost:2773";
   const path = "/systemsmanager/parameters/get/";
   const queryString = {
-    api_key:             `name=%2F${env}%2Ftwitter%2Fapi_key&withDecryption=true`,
-    api_key_secret:      `name=%2F${env}%2Ftwitter%2Fapi_key_secret&withDecryption=true`,
-    access_token:        `name=%2F${env}%2Ftwitter%2Faccess_token&withDecryption=true`,
-    access_token_secret: `name=%2F${env}%2Ftwitter%2Faccess_token_secret&withDecryption=true`
+    appKey:       `name=%2F${env}%2Ftwitter%2Fapi_key&withDecryption=true`,
+    appSecret:    `name=%2F${env}%2Ftwitter%2Fapi_key_secret&withDecryption=true`,
+    accessToken:  `name=%2F${env}%2Ftwitter%2Faccess_token&withDecryption=true`,
+    AccessSecret: `name=%2F${env}%2Ftwitter%2Faccess_token_secret&withDecryption=true`
   }
   const headers = {
     "X-Aws-Parameters-Secrets-Token": process.env.AWS_SESSION_TOKEN
@@ -17,45 +17,45 @@ import Twitter from "twitter-api-v2";
 
 export class TwitterService {
   twitterSecrets = {
-    api_key: "",
-    api_key_secret: "",
-    access_token: "",
-    access_token_secret: ""
+    appKey: "",
+    appSecret: "",
+    accessToken: "",
+    AccessSecret: ""
   }
 
   async config() {
     try {
-      const response = await fetch(`${endpoint}${path}?${queryString.api_key}`, { headers: headers });
+      const response = await fetch(`${endpoint}${path}?${queryString.appKey}`, { headers: headers });
       const json = await response.json();
-      this.twitterSecrets.api_key = json["Parameter"]["Value"];
+      this.twitterSecrets.appKey = json["Parameter"]["Value"];
     } catch (err) {
       console.error(err);
     }
     try {
-      const response = await fetch(`${endpoint}${path}?${queryString.api_key_secret}`, { headers: headers });
+      const response = await fetch(`${endpoint}${path}?${queryString.appSecret}`, { headers: headers });
       const json = await response.json();
-      this.twitterSecrets.api_key_secret = json["Parameter"]["Value"];
+      this.twitterSecrets.appSecret = json["Parameter"]["Value"];
     } catch (err) {
       console.error(err);
     }
     try {
-      const response = await fetch(`${endpoint}${path}?${queryString.access_token}`, { headers: headers });
+      const response = await fetch(`${endpoint}${path}?${queryString.accessToken}`, { headers: headers });
       const json = await response.json();
-      this.twitterSecrets.access_token = json["Parameter"]["Value"];
+      this.twitterSecrets.accessToken = json["Parameter"]["Value"];
     } catch (err) {
       console.error(err);
     }
     try {
-      const response = await fetch(`${endpoint}${path}?${queryString.access_token_secret}`, { headers: headers });
+      const response = await fetch(`${endpoint}${path}?${queryString.AccessSecret}`, { headers: headers });
       const json = await response.json();
-      this.twitterSecrets.access_token_secret = json["Parameter"]["Value"];
+      this.twitterSecrets.AccessSecret = json["Parameter"]["Value"];
     } catch (err) {
       console.error(err);
     }
   }
 
-  execute(messages) {
+  async execute(messages) {
     const client = new Twitter(this.twitterSecrets);
-    client.v2.tweetThread(messages).catch((err) => console.error(err));
+    await client.v2.tweetThread(messages).catch((err) => console.error(err));
   }
 }
